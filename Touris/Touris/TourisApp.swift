1c87c4d3bda0f231ab2972ca0b468a294f99e8f4
Arushi Goyal <52095341+arushi6131@users.noreply.github.com>
2024-07-26 18:34:45 -0700

    back to template code

--- Touris/Touris/TourisApp.swift
+++ Touris/Touris/TourisApp.swift
@@ -6,17 +6,10 @@
 //
 
 import SwiftUI
-import ArcGIS
-import ArcGISToolkit
 
 @main
 struct TourisApp: App {
-    
-    init() {
-        ArcGISEnvironment.apiKey = APIKey("AAPKfef7d1470143483bb582cd743db8bd98zPxkBR_WSA-MNtN1QDcuUQBYnk8qiLfSwNCh4ZPY_Ex87-DyXOVDDDeumrLYTQMl")
-    }
-    
-    var body: some SwiftUI.Scene {
+    var body: some Scene {
         WindowGroup {
             ContentView()
         }
