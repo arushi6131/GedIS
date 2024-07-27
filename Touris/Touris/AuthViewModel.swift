8a3ea15b5df7b7d2d0f850a69465f21f9a63cdc1
Jaysen Gomez <jaysengomez@esri.com>
2024-07-26 18:49:43 -0700

    AuthViewModel

---
+++ Touris/Touris/AuthViewModel.swift
@@ -0,0 +1,34 @@
+//
+//  AuthViewModel.swift
+//  Touris
+//
+//  Created by Jaysen Gomez on 7/26/24.
+//
+
+import Foundation
+import FirebaseAuth
+import FirebaseFirestore
+import FirebaseFirestoreSwift
+class AuthViewModel: ObservableObject {
+  @Published var errorMessage = ""
+  @Published var currentUserID = Auth.auth().currentUser?.uid
+  @Published var isSignedIn = false
+  func signIn(email: String, password: String) {
+    // Reset error message
+    errorMessage = ""
+    Auth.auth().signIn(withEmail: email, password: password) { (result, error) in
+      if let error = error {
+        self.errorMessage = error.localizedDescription
+      } else {
+        self.isSignedIn = true
+        self.currentUserID = result?.user.uid
+      }
+    }
+  }
+  func createAccount(email: String, password: String) {
+    // Reset error message
+    errorMessage = ""
+    Auth.auth().createUser(withEmail: email, password: password) { (result, error) in
+      if let error = error {
+        self.errorMessage = error.localizedDescription
+      }
